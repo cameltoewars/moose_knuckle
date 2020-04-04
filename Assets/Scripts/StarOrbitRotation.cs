bddbd13ec600332064548ca93395acc2a6157596
tomslat <36338107+tomslat@users.noreply.github.com>
2020-04-04 21:25:50 +0100

    Added another planet, planet rotation and Star orbit.

---
+++ Assets/Scripts/StarOrbitRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarOrbitRotation : MonoBehaviour
+{
+    public float orbitSpeed;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 orbitOriginPoint = new Vector3(500, 0, 0);
+        Vector3 axis = new Vector3(0, 0, 1);
+        transform.RotateAround(orbitOriginPoint, axis, orbitSpeed * Time.deltaTime);
+    }
+}
